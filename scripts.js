c4d387e28c52be1c936770002eefd1b796b9a42a
peterdbinvesting <peter.s@dbinvesting.com>
2025-06-14 23:10:18 +0300

    implement dark mode functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,41 @@
 function toggleMobileMenu() {
   document.getElementById("menu").classList.toggle("active");
 }
+
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+const iconMoon = darkModeToggle.querySelector(".moon");
+const iconSun = darkModeToggle.querySelector(".sun");
+
+function setTheme(theme) {
+  if (theme === "dark") {
+    body.classList.add("dark-mode");
+    iconMoon.classList.remove("active");
+    iconSun.classList.add("active");
+  } else {
+    body.classList.remove("dark-mode");
+    iconSun.classList.remove("active");
+    iconMoon.classList.add("active");
+  }
+  localStorage.setItem("theme", theme);
+}
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  setTheme(savedTheme);
+} else if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  setTheme("dark");
+} else {
+  setTheme("light");
+}
+
+darkModeToggle.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    setTheme("light");
+  } else {
+    setTheme("dark");
+  }
+});
